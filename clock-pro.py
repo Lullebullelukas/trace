fb1c1daf77e46ca56931d9ded8f14159633b75f6
toraelding <tora.larsson@neo4j.com>
2023-11-14 15:38:04 +0100

    should be correct clock-pro

--- clock-pro.py
+++ clock-pro.py
@@ -52,7 +52,6 @@ class ClockPro:
             self.refs[index] = 0
 
             if page in self.test_pages and ref > 0:
-                self.test_pages.discard(page)
                 #it was in test period when got it, promote to hot
                 self.hot_pages.add(page)
                 if len(self.hot_pages) > self.capacity_hot:
@@ -96,6 +95,8 @@ class ClockPro:
                 #remove non res if we see one
                 self.non_res_pages.discard(non_res_page)
                 self.non_res_cache[index] = None 
+                if non_res_page in self.test_pages:
+                    self.test_pages.discard(non_res_page)
 
     def move_test_hand(self):
         removed = False
@@ -108,6 +109,8 @@ class ClockPro:
                 self.non_res_pages.discard(non_res_page)
                 self.non_res_cache[index] = None
                 removed = True
+                if non_res_page in self.test_pages:
+                    self.test_pages.discard(non_res_page)
             if page in self.test_pages:
                 self.test_pages.discard(page)
 
